312989d84a1247aeb62c9e8c7c5933701b31fd0d
artur <agartim@gmail.com>
2018-01-15 23:35:14 +0100

    Add some views in layout.

--- app/src/main/java/agartim/pl/arrivalconfirmation/MainActivity.kt
+++ app/src/main/java/agartim/pl/arrivalconfirmation/MainActivity.kt
@@ -1,22 +1,36 @@
 package agartim.pl.arrivalconfirmation
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var sharedPreferences: SharedPreferences
+    val PREFS_MESSAGE: String = "sms_message"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+
+        message.setText(sharedPreferences.getString(PREFS_MESSAGE, ""))
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        send.setOnClickListener { view ->
+            Snackbar.make(view, "Send sms to receivers", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
@@ -36,4 +50,12 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    override fun onPause() {
+        super.onPause()
+        readAndSaveMessage()
+    }
+
+    private fun readAndSaveMessage() = sharedPreferences.edit().putString(PREFS_MESSAGE, message.text.toString()).apply()
+
 }
